089f94ad4e9754a294670b1d62ce964f9e324d79
J.J. Gomez-Cadenas <jjgomezcadenas@gmail.com>
2020-06-05 15:21:09 +0200

    Adding functionality to the function

--- useir/analysis.py
+++ useir/analysis.py
@@ -1,6 +1,80 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-def plot_useir(dfs, lbls, T = 'uSEIR', figsize=(10,10)):
+def useir_imax(dfs):
+    I = dfs.I.values
+    return np.max(I), np.argmax(I)
+
+
+def useir_simax(dfs):
+    SI = dfs.I.values * dfs.S.values
+    return np.max(SI), np.argmax(SI)
+
+
+def useir_s_at_simax(dfs):
+    _, tsi = useir_simax(dfs)
+    return dfs.S.values[tsi]
+
+
+def useir_i_above_thr(dfs, thr=0.01):
+    I = dfs.I.values
+    return np.argmax(I>thr)
+
+
+
+def useir_compared(quenched = False, I0 = 1e-3, r0 = 3.5, ti = 5.5, tr = 6.5, r1=0.5, tq = 50, eps= 0.01, dim = 20000):
+
+    pdf = csolve_uSeir(dist = 'poisson',
+                       quenched = quenched,
+                       I0       = I0,
+                       ti_shape = ti,
+                       tr_shape = tr,
+                       R0       = r0,
+                       R1       = r1,
+                       tq       = tq,
+                       eps      = eps,
+                       dim      = dim
+                  )
+
+    gdf = csolve_uSeir(dist = 'gamma',
+                       quenched = quenched,
+                       I0       = I0,
+                       ti_shape = ti,
+                       tr_shape = tr,
+                       ti_scale = 1,
+                       tr_scale = 1,
+                       R0       = r0,
+                       R1       = r1,
+                       tq       = tq,
+                       eps      = eps,
+                       dim      = dim
+                  )
+
+    edf = csolve_uSeir(dist = 'expon',
+                       quenched = quenched,
+                       I0       = I0,
+                       ti_scale = ti,
+                       tr_scale = tr,
+                       R0       = r0,
+                       R1       = r1,
+                       tq       = tq,
+                       eps      = eps,
+                       dim      = dim
+                  )
+    return gdf, edf, pdf
+
+
+def useir_imax_and_i_above_thr_compared(pdfs, tpdfs, thr=0.001, eps=0.01):
+
+    for i, dfs in enumerate(pdfs):
+        imax, tmax = useir_imax(dfs)
+        th         =useir_i_above_thr(dfs, thr)
+        print(f'for distributions {tpdfs[i]}, imax = {imax}, tmax = {tmax}, t > th ({thr}) = {th * eps}')
+
+
+
+def plot_useir(dfs, lbls, lines, T = 'uSEIR', xlim=(0,100), ylim=(0,0.3),figsize=(10,10)):
 
     fig = plt.figure(figsize=figsize)
 
@@ -9,10 +83,11 @@ def plot_useir(dfs, lbls, T = 'uSEIR', figsize=(10,10)):
         df.head()
         ls = f'S-{lbls[i]}'
         lr = f'R-{lbls[i]}'
-        plt.plot(df.t, df.S, lw=2, label=ls)
-        plt.plot(df.t, df.R, lw=2, label=lr)
+        plt.plot(df.t, df.S, lw=2, linestyle=lines[i],label=ls)
+        plt.plot(df.t, df.R, lw=2, linestyle=lines[i], label=lr)
 
-
+    plt.xlim(*xlim)
+    plt.ylim(0,1)
     plt.xlabel('time (days)')
     plt.ylabel('Fraction of population')
     plt.legend()
@@ -22,12 +97,37 @@ def plot_useir(dfs, lbls, T = 'uSEIR', figsize=(10,10)):
     for i, df in enumerate(dfs):
         le = f'E-{lbls[i]}'
         li = f'I-{lbls[i]}'
-        plt.plot(df.t, df.E, lw=2, label=le)
-        plt.plot(df.t, df.I, lw=2, label=li)
+        #plt.plot(df.t, df.E, lw=2, linestyle=lines[i], label=le)
+        plt.plot(df.t, df.I, lw=2, linestyle=lines[i], label=li)
+
+    plt.xlim(*xlim)
+    plt.ylim(*ylim)
+    plt.xlabel('time (days)')
+    plt.ylabel('Fraction of population')
+    plt.legend()
+
+    plt.title(T)
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_useir_IS(dfs, lbls, T = 'uSEIR', figsize=(10,10)):
+
+    fig = plt.figure(figsize=figsize)
+
+    ax=plt.subplot(1,1,1)
+    for i, df in enumerate(dfs):
+        df.head()
+        ls = f'S-{lbls[i]}'
+        lr = f'R-{lbls[i]}'
+        plt.plot(df.t, df.S * df.I, lw=2, label=ls)
+        plt.plot(df.t, df.S, lw=2, linestyle='dashed',label=ls)
+
 
     plt.xlabel('time (days)')
     plt.ylabel('Fraction of population')
     plt.legend()
+    plt.title(T)
 
     plt.title(T)
     plt.tight_layout()
--- useir/useir.pyx
+++ useir/useir.pyx
@@ -115,7 +115,10 @@ cpdef csolve_uSeir(str dist            = 'gamma',
                    double tr_shape     = 6.5,
                    double tr_scale     = 1,
                    str    pde_sampling = 'Fine',
-                   double R0           = 3.5):
+                   double R0           = 3.5,
+                   double Smin         = 1e-12,
+                   double Emin         = 1e-12,
+                   double nmax         = 75000):
     """
     Function csolve_uSeir solves numerically the uSEIR equations (Hernandez et al, Preprint)
     following the numerical recipe by A. Ramos ( https://gitlab.ift.uam-csic.es/alberto/useir)
@@ -136,10 +139,7 @@ cpdef csolve_uSeir(str dist            = 'gamma',
     The default sampling is Fine, which takes a epsilon time of 0.01. For faster execution specify Coarse
     """
 
-    cdef double N       = 1e+6,
-    cdef double Smin    = 1e-10,
-    cdef double Emin    = 1e-10,
-    cdef double nmax    = 75000
+    cdef double N       = 1e+6
     cdef double eps
     cdef double pn
     cdef double prob
@@ -214,3 +214,120 @@ cpdef csolve_uSeir(str dist            = 'gamma',
     return pd.DataFrame({'t': TT.astype(float), 'S': SS.astype(float),'E' : EE.astype(float), 'I' : II.astype(float), 'R': RR.astype(float)})
     #return TT.astype(float), SS.astype(float), EE.astype(float), II.astype(float), RR.astype(float)
     #return TT, SS, EE, II, RR
+
+
+
+cpdef csolve_uSeir_quenched(str dist            = 'gamma',
+                                double ti_shape     = 5.5,
+                                double ti_scale     = 1,
+                                double tr_shape     = 6.5,
+                                double tr_scale     = 1,
+                                str    pde_sampling = 'Fine',
+                                double R0           = 3.5,
+                                double R1           = 0.5,
+                                double tq           = 30):
+        """
+        Function csolve_uSeir solves numerically the uSEIR equations (Hernandez et al, Preprint)
+        following the numerical recipe by A. Ramos ( https://gitlab.ift.uam-csic.es/alberto/useir)
+
+        THE BEER-WARE LICENSE:
+        P. Hernandez, C. Pena, J.J. Gomez Cadenas wrote these files. As long
+        as you retain this notice you can do whatever you want with this
+        stuff. If we meet some day, and you think this stuff is worth it, you
+        can buy us a beer in return.
+
+        Arguments:
+        dist       : either gamma, expon or poisson. Other distributions can easily be added.
+        shape and scale parameters:
+                   for gamma you need to specify both --ti_shape, ti_scale, tr_shape, tr_scale
+                   for expon you need to specify only ti_scale, tr_scale (which correspond to the average ti, tr)
+                   for poisson you need to specify only ti_shape, tr_shape (which correspond to mu for the average ti, tr)
+
+        The default sampling is Fine, which takes a epsilon time of 0.01. For faster execution specify Coarse
+        """
+
+        cdef double N       = 1e+6,
+        cdef double Smin    = 1e-12,
+        cdef double Emin    = 1e-12,
+        cdef double nmax    = 50000
+        cdef double eps
+        cdef double pn
+        cdef double prob
+
+        cdef int dim
+        cdef int nE
+        cdef int nI
+
+        cdef double [:] pdE
+        cdef double [:] pdI
+
+        nE, pdE, nI, pdI, ti, tr = compute_pde(dist, ti_shape, ti_scale, tr_shape, tr_scale, pde_sampling)
+
+        cdef double [:] I   = np.zeros(nI, dtype=np.double)
+        cdef double [:] E   = np.zeros(nE, dtype=np.double)
+        E[0] = 1 / N
+
+
+        cdef double S   = 1 - 1/N
+        cdef double R   = 0
+        cdef double sI  = 0
+
+
+        eps, dim = compute_eps(pde_sampling)
+        prob = R0 / tr
+        pn = prob * eps
+
+        print(f'prob (R0 = {R0})= {prob}, pn = {pn}')
+
+        cdef np.ndarray[np.float64_t, ndim=1] TT = np.zeros(dim, dtype=np.double)
+        cdef np.ndarray[np.float64_t, ndim=1] SS = np.zeros(dim, dtype=np.double)
+        cdef np.ndarray[np.float64_t, ndim=1] EE = np.zeros(dim, dtype=np.double)
+        cdef np.ndarray[np.float64_t, ndim=1] II = np.zeros(dim, dtype=np.double)
+        cdef np.ndarray[np.float64_t, ndim=1] RR = np.zeros(dim, dtype=np.double)
+
+        cdef int n    = 0
+        cdef int end
+        cdef int k
+        while True:
+            t  = n * eps
+
+            if t < tq:
+                prob = R0 / tr
+            else:
+                prob = R1 / tr
+            pn = prob * eps
+
+            R += I[0]
+            end = nI - 1
+            for k in range(end):
+                I[k] = I[k+1] + pdI[k] * E[0]
+            I[end] = pdI[end] * E[0]
+
+            #print(I)
+
+            end = nE - 1
+            for k in range(end):
+                E[k] = E[k+1] + pn * pdE[k] * sI * S
+            E[end]   = pn * pdE[end] * sI * S
+
+            #print(E)
+
+            S  = S - pn * sI * S
+            sI = np.sum(I)
+            sE = np.sum(E)
+
+            TT[n] = n * eps
+            SS[n] = S
+            EE[n] = sE
+            II[n] = sI
+            RR[n] = R
+
+
+            #print(f't = {n*eps} S = {S} E ={sE} I ={sI} R = {R}')
+            n+=1
+            if (sE < Smin and sI < Emin) or n > dim -1:
+                break
+
+        return pd.DataFrame({'t': TT.astype(float), 'S': SS.astype(float),'E' : EE.astype(float), 'I' : II.astype(float), 'R': RR.astype(float)})
+        #return TT.astype(float), SS.astype(float), EE.astype(float), II.astype(float), RR.astype(float)
+        #return TT, SS, EE, II, RR
